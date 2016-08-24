befdfad8fbf3b47f27f5ffaf4454e8c9108f7801
lzan13 <lzan13.gg@gmail.com>
2016-08-24 20:39:33 +0800

    修改初始化方法

--- app/src/main/java/net/melove/demo/easechat/ECApplication.java
+++ app/src/main/java/net/melove/demo/easechat/ECApplication.java
@@ -51,11 +51,24 @@ public class ECApplication extends Application {
         if (isInit) {
             return;
         }
-        /**
-         * SDK初始化的一些配置
-         * 关于 EMOptions 可以参考官方的 API 文档
-         * http://www.easemob.com/apidoc/android/chat3.0/classcom_1_1hyphenate_1_1chat_1_1_e_m_options.html
-         */
+
+        // 调用初始化方法初始化sdk
+        EMClient.getInstance().init(mContext, initOptions());
+
+        // 设置开启debug模式
+        EMClient.getInstance().setDebugMode(true);
+
+        // 设置初始化已经完成
+        isInit = true;
+    }
+
+    /**
+     * SDK初始化的一些配置
+     * 关于 EMOptions 可以参考官方的 API 文档
+     * http://www.easemob.com/apidoc/android/chat3.0/classcom_1_1hyphenate_1_1chat_1_1_e_m_options.html
+     */
+    private EMOptions initOptions() {
+
         EMOptions options = new EMOptions();
         // 设置Appkey，如果配置文件已经配置，这里可以不用设置
         // options.setAppKey("lzan13#hxsdkdemo");
@@ -63,10 +76,12 @@ public class ECApplication extends Application {
         options.setAutoLogin(true);
         // 设置是否需要发送已读回执
         options.setRequireAck(true);
-        // 设置是否需要发送回执，TODO 这个暂时有bug，上层收不到发送回执
+        // 设置是否需要发送回执，
         options.setRequireDeliveryAck(true);
         // 设置是否需要服务器收到消息确认
         options.setRequireServerAck(true);
+        // 设置是否根据服务器时间排序，默认是true
+        options.setSortMessageByServerTime(false);
         // 收到好友申请是否自动同意，如果是自动同意就不会收到好友请求的回调，因为sdk会自动处理，默认为true
         options.setAcceptInvitationAlways(false);
         // 设置是否自动接收加群邀请，如果设置了当收到群邀请会自动同意加入
@@ -80,14 +95,7 @@ public class ECApplication extends Application {
         // 设置集成小米推送的appid和appkey
         // options.setMipushConfig(MLConstants.ML_MI_APP_ID, MLConstants.ML_MI_APP_KEY);
 
-        // 调用初始化方法初始化sdk
-        EMClient.getInstance().init(mContext, options);
-
-        // 设置开启debug模式
-        EMClient.getInstance().setDebugMode(true);
-
-        // 设置初始化已经完成
-        isInit = true;
+        return options;
     }
 
     /**
